7e60d9fa8c3e8eb802f5889158236de006f87c85
isaac barcroft <i_barcroft@yahoo.com>
2025-10-09 10:11:15 -0400

    utils

---
+++ src/utils/colors.ts
@@ -0,0 +1,10 @@
+export const stringToColor = (str: string)=> {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  }
+  const color = Math.floor(
+    Math.abs(Math.sin(hash) * 16777215) % 16777215
+  ).toString(16)
+  return `#${"000000".substring(0, 6 - color.length)}${color}`
+}
