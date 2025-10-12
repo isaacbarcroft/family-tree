c6de4a4983f22218c22424ac3eef50eb2872fecf
isaac barcroft <i_barcroft@yahoo.com>
2025-10-12 13:37:33 -0400

    update enums

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,4 +1,4 @@
-export const ROLE_TYPES = ["member", "friend", "neighbor", "pastor", "other"] as const
+export const ROLE_TYPES = ["family member", "friend", "neighbor", "pastor", "other"] as const
 export type RoleType = (typeof ROLE_TYPES)[number]
 
 export const EVENT_TYPES = ["life", "memory", "historical"] as const
